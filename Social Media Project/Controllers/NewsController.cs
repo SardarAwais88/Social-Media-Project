8167e27ed1b136955c1ea3101dc026c5da175431
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-10-15 22:12:05 -0700

    now we add a news controller

---
+++ Social Media Project/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Social_Media_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+  /*      public RegistrationController(IConfiguration configuration)
+        {
+            
+        }*/
+        public  NewsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
