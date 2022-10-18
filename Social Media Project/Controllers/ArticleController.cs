9f90eddfee927998e3eae2d111b77b7a494b88a4
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-10-17 19:57:55 -0700

    now we add article controlleer and modify it thanks

---
+++ Social Media Project/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Social_Media_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        
+    }
+}
