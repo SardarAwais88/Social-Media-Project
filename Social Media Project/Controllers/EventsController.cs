9ff5d5322bab67e8274b1cfc63536d6b9d977223
Sardar <Sardar@DESKTOP-5J0AHU4>
2022-10-24 09:45:46 -0800

    now add another events controller to start it we just copy news controller and modify it as we need

---
+++ Social Media Project/Controllers/EventsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Social_Media_Project.Models;
+
+namespace Social_Media_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+  /*      public RegistrationController(IConfiguration configuration)
+        {
+            
+        }*/
+        public  EventsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("AddEvents")]
+
+        public Response AddNews(News news)
+        {
+            Response response = new Response();
+            // create sql connect
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SnCon").ToString());
+
+            Dal dal = new Dal();
+            response = dal.AddNews(news, connection);
+            return response;
+        }
+
+
+        [HttpGet]
+        [Route("NewsList")]
+
+        public Response NewsList()
+        {
+            Response response = new Response();
+            // create sql connect
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SnCon").ToString());
+
+            Dal dal = new Dal();
+            response = dal.NewsList( connection);
+            return response;
+        }
+    }
+}
